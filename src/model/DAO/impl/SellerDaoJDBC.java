7b3650c69a1eec9af97e2a2af46b12025d11b05f
Thalys Ravel <ravelthalys66@proton.me>
2025-12-17 19:50:07 -0300

    Added SellerDaoJDBC and DaoFactory

---
+++ src/model/DAO/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.DAO.impl;
+
+import java.util.List;
+
+import model.DAO.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller s) {
+        
+    }
+
+    @Override
+    public void update(Seller s) {
+        
+    }
+
+    @Override
+    public void deletedById(Integer id) {
+        
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
