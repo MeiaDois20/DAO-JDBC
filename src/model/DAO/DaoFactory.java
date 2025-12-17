32cd0c360922e88f8361959178ec72c1b1ba365f
Thalys Ravel <ravelthalys66@proton.me>
2025-12-17 20:23:51 -0300

    findById implementation

--- src/model/DAO/DaoFactory.java
+++ src/model/DAO/DaoFactory.java
@@ -1,13 +1,10 @@
 package model.DAO;
 
+import db.DB;
 import model.DAO.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
-
-    // public static DepartmentDao createDepartmentDao() {
-    //     return new DepartmentDaoJDBC();
-    // }
 }
--- src/model/DAO/impl/SellerDaoJDBC.java
+++ src/model/DAO/impl/SellerDaoJDBC.java
@@ -1,11 +1,26 @@
 package model.DAO.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.naming.spi.DirStateFactory.Result;
+
+import db.DB;
+import db.DbException;
 import model.DAO.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
+
+    private Connection conn;
+
+    public SellerDaoJDBC (Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller s) {
@@ -24,7 +39,41 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = DB.getConnection().prepareStatement(
+                "SELECT seller.*,department.Name as DepName " +
+                "FROM seller INNER JOIN department " +
+                "ON seller.DepartmentId = department.Id " +
+                "WHERE seller.Id = ?"
+            );
+
+            ps.setInt(1, id);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closePreparedStatement(ps);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
